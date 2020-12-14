7ed878c2d4c133d136b0cbfa0919d91b7843a9e2
araatiche <araatiche@yandex.ru>
2020-12-14 16:08:39 +0300

    исправлены замечания

--- caclulator.py
+++ caclulator.py
@@ -1,19 +1,23 @@
-FirstNumber = int(input("Введите первое число: ")) # записываем ввод первого числа в переменную FirstNumber
+first_number = int(input("Введите первое число: ")) # записываем ввод первого числа в переменную first_number
 print("Введите символ, какую операцию хотитие выполнить.")
 print("Допустимые символы операции: +, -, *, /")
-Operation = input("Введите символ: ") # записываем ввод символа операции в переменную Operation
-SecondNumber = int(input("Введите второе число: ")) # записываем ввод второго числа в переменную SecondNumber
+operation = input("Введите символ: ") # записываем ввод символа операции в переменную operation
+second_number = int(input("Введите второе число: ")) # записываем ввод второго числа в переменную second_number
 #
 # в зависимости от введеного символа выбераем соответствующую операцию над числами:
-if Operation == "+":
-    Result = FirstNumber + SecondNumber
-elif Operation == "-":
-    Result = FirstNumber - SecondNumber
-elif Operation == "*":
-    Result = FirstNumber * SecondNumber
-elif Operation == "/":
-    Result = FirstNumber / SecondNumber
-elif Operation:
+if operation == "+":
+    result = first_number + second_number
+elif operation == "-":
+    result = first_number - second_number
+elif operation == "*":
+    result = first_number * second_number
+elif operation == "/":
+    if second_number == 0: # проверка деления на 0
+        print("Деление на 0!") # если есть, выводим сообщение и выходим из программы
+        exit()
+    else:
+        result = first_number / second_number
+elif operation:
     print("Введен некоректный символ операции!") # в случае если введен некоректный символ операции выдаем соответвующее сообщение
     exit()                                       # и досрочно завершаем программу
-print("Результат операции '",Operation,"':",str(Result)) # если все правильно выдаем результат
+print("Результат операции '",operation,"':",str(result)) # если все правильно выдаем результат
